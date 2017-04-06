aee20a42b935126d2e4540e5313d279f266f82b3
Bryan Stephens <bryan.stephens@infusionsoft.com>
2017-04-06 16:24:00 -0700

    Removing reference to CD team
    The CD team as it existed is no more. Instead the SRE team is
    responsible for build related topics.

--- circle/deploy.sh
+++ circle/deploy.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-git config user.email "cd@infusionsoft.com"
+git config user.email "sre@infusionsoft.com"
 git config user.name "CircleCI"
 $M2/mvn release:prepare release:perform --batch-mode
